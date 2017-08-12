2cbb9184f85f6672534be301e2941cafd32dea49
QAlexBall <zhuderen12345@icloud.com>
2017-08-12 21:06:03 +0800

    add a idea project for study Java

---
+++ src/unit4/ClockTestDrive.java
@@ -0,0 +1,23 @@
+package unit4;
+
+class Clock {
+    private String time;
+
+    void setTime(String t) {
+        time = t;
+    }
+    String getTime() {
+        return time;
+    }
+}
+public class ClockTestDrive {
+    public static void main(String [] args) {
+
+        Clock c = new Clock();
+
+        c.setTime("1245");
+        String tod = c.getTime();
+        System.out.println("time" + tod);
+
+    }
+}
