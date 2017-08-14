abb25b7d64e035e42bd007d367be13869ff9f27c
QAlexBall <zhuderen12345@icloud.com>
2017-08-14 23:31:42 +0800

    done a SimpleDotCom Game project

---
+++ src/unit5/SimpleDotCom.java
@@ -0,0 +1,26 @@
+package unit5;
+
+public class SimpleDotCom {
+    int [] locationCells;
+    int numOfHits = 0;
+
+    public void setLocationCells(int[] locationCells) {
+        this.locationCells = locationCells;
+    }
+    public String checkYourself(String strinGuess) {
+        int guess = Integer.parseInt(strinGuess);
+        String result = "miss";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }// out of the loop
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        }
+        System.out.println(result);
+        return result;
+    }
+}
