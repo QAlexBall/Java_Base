dee323d6173a3bc421dbf4b432a9dc0392850432
QAlexBall <zhuderen12345@icloud.com>
2017-08-27 23:21:46 +0800

    add AnimalTestDrive to unit8

---
+++ src/unit8/AnimalTestDrive.java
@@ -0,0 +1,35 @@
+/*
+Author: QAlexBall
+Date: 2017/8/27
+Contents: create an general animal list "MyAnimalList" and test
+ */
+package unit8;
+class MyAnimalList {
+    private Animal[] animals = new Animal[5];// save the animals array object
+    private int nextIndex = 0;
+    public void add(Animal a) {
+        if (nextIndex < animals.length) {
+            animals[nextIndex] = a;
+            System.out.println("Animal added at " + nextIndex + "  " + a);
+            nextIndex++;
+        }//close if
+    }//close ass
+}
+abstract class Animal {
+}
+
+class Dog extends Animal {
+}
+
+class Cat extends Animal {
+}
+
+public class AnimalTestDrive {
+    public static void main(String[] args) {
+        MyAnimalList list = new MyAnimalList();
+        Dog a = new Dog();
+        Cat c = new Cat();
+        list.add(a);
+        list.add(c);
+    }
+}
